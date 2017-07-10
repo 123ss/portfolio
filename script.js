a31ffc41efe529377ea0b2695388a89587794927
123ss <selena_sui@yahoo.com>
2017-07-10 13:56:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+document.getElementById("toggle").onclick = function toggle {
+  document.getElementsByTagName("body").style.backgroundColor = "black";
+  document.getElementsByTagName("body").style.color = "white";
+}

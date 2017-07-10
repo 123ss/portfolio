3d5c6c489bf922f1d115945e2130be1f7cc0f245
123ss <selena_sui@yahoo.com>
2017-07-10 14:14:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.getElementById("toggle").onclick = function(){toggle()};
 
 function toggle(){
   console.log("hello");
+ 
+  document.getElementById("toggle").innerHTML = "black";
+  
   document.getElementsByTagName("body").style.backgroundColor = "black";
   document.getElementsByTagName("body").style.color = "white";
 }
